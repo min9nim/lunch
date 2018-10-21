2d7890e9ae46372c700d1783b7331987007f0764
songmingu(mac) <min9nim@gmail.com>
2018-10-22 01:08:24 +0900

    update

--- src/com/app.js
+++ src/com/app.js
@@ -33,67 +33,11 @@ const initData = async () => {
     }catch{
       app.state.data = [];
     }
+
+    // 상태변화에 따른 UI변화 코딩은 mobX로 제거 가능
     app.view.List && app.view.List.setState({data : app.state.data});
 }
 
-
-app.bodyScroll = function () {
-  
-  // 목록화면이 아니면 리턴  
-  if(app.thispage !== "List") return;
-
-  // 현재 목록화면 scrollTop 의 값
-  const scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
-  
-
-  // 현재 스크롤 값을 전역변수에 저장
-  app.scrollTop = scrollTop;
-
-  if(app.isScrollLast) return;
-  // 아직 모든 글이 로드된 상태가 아니라면 스크롤이 아래까지 내려왔을 때 다음 글 10개 로드
-
-  //현재문서의 높이
-  const scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-  //현재 화면 높이 값
-  const clientHeight = document.documentElement.clientHeight;
-
-  console.log("scrollTop : " + scrollTop)
-  console.log("clientHeight : " + clientHeight)
-  console.log("scrollHeight : " + scrollHeight)
-
-
-  if (
-    (scrollTop + clientHeight === scrollHeight)    // 일반적인 경우(데스크탑: 크롬/파폭, 아이폰: 사파리)
-    ||
-    (app.isMobileChrome() && (scrollTop + clientHeight === scrollHeight - 56))   // 모바일 크롬(55는 위에 statusbar 의 높이 때문인건가)
-  ){ //스크롤이 마지막일때
-  
-  /*
-  * 18.09.19 min9nim
-  * 아래와 같이 분기 처리하면 데스크탑 크롬에서 스크롤이 마지막에 닿고나서 요청이 여러번 한꺼번에 올라가는 문제 발생
-  * //if ((scrollTop + clientHeight) >= scrollHeight-55) { 
-  */
-
-    nprogress.start();
-    $m("#nprogress .spinner").css("top", "95%");
-    app.api.getPosts({
-        //idx: app.view.App.state.data.posts.length,
-        idx: app.store.getState().data.posts.filter(p => p.origin === undefined).length,
-        cnt: PAGEROWS,
-        search: app.store.getState().view.search,
-        hideProgress: true,
-        context: app.context
-      })
-      .then(res => {
-        app.store.dispatch(app.action.scrollEnd(res.posts));
-        if(res.posts.length < PAGEROWS){
-          console.log("Scroll has touched bottom")
-          app.isScrollLast = true;
-          return;
-        }
-      })
-  }
-};
 
 
 app.checkStatus = function(res){
@@ -148,14 +92,6 @@ app.isMobileChrome = function(){
   return !app.isDesktop() && navigator.userAgent.includes("Chrome");
 }
 
-
-app.highlight = function(txt, word){
-  if(word){
-      var reg = new RegExp("(" + word + ")", "gi");
-      txt = txt.replace(reg, '<span style="background-color:yellow;">$1</span>');
-  }
-  return txt;
-}
 
 
 //Ref) https://gist.github.com/acdlite/a68433004f9d6b4cbc83b5cc3990c194
